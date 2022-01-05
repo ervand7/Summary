df604cc26c2ebf2cbad720276ff00bc0ec2ff2d3
ervand7 <ervand7@mail.ru>
2022-01-05 13:36:03 +0300

    05.01.2022

---
+++ UNIX/Scripts/язык bash/tutorial/multi-line_comments.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+: '
+this is a multi-line comment
+this is a multi-line comment
+this is a multi-line comment
+this is a multi-line comment
+this is a multi-line comment
+this is a multi-line comment
+this is a multi-line comment
+this is a multi-line comment
+this is a multi-line comment'
+
+echo "Hello"

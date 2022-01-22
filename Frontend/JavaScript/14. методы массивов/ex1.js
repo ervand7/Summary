21c25810efa5b12ffb6bbfa0fac1d7c2594e17b0
ervand7 <ervand7@mail.ru>
2022-01-22 12:49:23 +0300

    22.01.2022

--- Frontend/JavaScript/14. методы массивов/ex1.js
+++ Frontend/JavaScript/14. методы массивов/ex1.js
@@ -74,14 +74,90 @@ digs.forEach((i, index) => {
 6 с индексом 5
  */
 
-// ========== indexOf, lastIndexOf, invludes ==========
+// ========== indexOf, lastIndexOf, includes ==========
 let a = ['Я', 'смотрю', 'этот', 'обучающий', 'урок', 0, false, null]
-// найдем элемент 'смотрю', начиная с 0 индекса
+// найдем индекс элемента 'смотрю', начиная с 0 индекса
 let res1 = a.indexOf('смотрю', 0)
 console.log(res1)  // 1
-// найдем элемент null, начиная с 0 индекса м после этого идем в самое начало массива
+// найдем индекс элемента null, начиная с 0 индекса м после этого идем в самое начало массива
 let res2 = a.lastIndexOf(null, 0)
 console.log(res2)  // -1 (не найдено)
 // проверим, есть элемент 0, начиная с 3 индекса
 let res3 = a.includes(0, 3)
 console.log(res3)  // true
+
+
+// ========== find, findIndex ==========
+// эти методы ищут только первый подходящий элемент
+let cars = [
+    {model: 'toyota', price: 1000},
+    {model: 'opel', price: 800},
+    {model: 'reno', price: 1200},
+]
+// найдем обхект, у которого поле price < 1000
+let rez = cars.find(i => i.price < 1000)
+console.log(rez)  // {model: 'opel', price: 800}
+
+// метод findIndex делает все то же самое, но только возвращает не сам объект, а индекс
+let indexRez = cars.findIndex(i => i.price < 1000)
+console.log(indexRez)  // 1
+
+// ========== filter ==========
+// делает то же самое, что и find, но только ищет не первое, а все подходящие элементы
+let filterRez = cars.filter(i => i.price <= 1000)
+console.log(filterRez)  // [{model: "toyota", price: 1000}, {model: "opel", price: 800}]
+
+
+// ========== map ==========
+let myCars = ['toyota', 'opel', 'reno']
+let result = myCars.map(function (i) {
+    return i.length
+})
+console.log(result)  // [6, 4, 4]
+
+
+// ========== sort ==========
+let myDigs = [4, 25, 2]
+myDigs.sort()  // результат будет некорректным, так как sort воспринимает все как строки
+console.log(myDigs)  // [2, 25, 4]
+
+// подправим это
+myDigs.sort(
+    function (a, b) {
+        if (a > b) return 1  // можно вообще указать любое положительное число
+        else if (a < b) return -1  // можно вообще указать любое отрицательное число
+        else return 0
+    }
+)
+console.log(myDigs)  // [2, 4, 25]
+
+// можно переписать еще удобнее через стрелочную функцию
+myDigs.sort( (a, b) => a-b)
+console.log(myDigs)  // [2, 4, 25]
+
+
+// ========== reverse ==========
+let someDigs = [1, 4, 2, 7, 5, 9]
+someDigs.reverse()
+console.log(someDigs)  // [9, 5, 7, 2, 4, 1]
+
+
+// ========== split/join ==========
+let emailsTo = 'alex12@m.ru, hello@m.ru, world@m.ru, hi@m.ru'
+let arEmails = emailsTo.split(', ', 3)
+for (let email of arEmails)
+    console.log(email)
+/*
+alex12@m.ru
+hello@m.ru
+world@m.ru
+ */
+
+let strEmails = arEmails.join(', ')
+console.log(strEmails)  // 'alex12@m.ru, hello@m.ru, world@m.ru'
+
+
+// ========== reduce/reduceRight ==========
+let my_digs = [1, 4, 2, 7, 5, 9]
+let mySum = my_digs.reduce((sum, current) => sum + current, 0)  // 0 - это начальное значение суммы
+console.log(mySum)  // 28

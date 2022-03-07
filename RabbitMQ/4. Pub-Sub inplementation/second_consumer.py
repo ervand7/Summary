f5ff9a9841f783bead10a420a37e48c8e884aa2c
ervand7 <ervand7@mail.ru>
2022-03-07 14:19:22 +0300

    07.03.2022

---
+++ RabbitMQ/4. Pub-Sub inplementation/second_consumer.py
@@ -0,0 +1,19 @@
+import pika
+from pika.exchange_type import ExchangeType
+
+
+def on_message_received(ch, method, properties, body):
+    print(f'secondconsumer: received new message: {body}')
+
+
+connection_parameters = pika.ConnectionParameters('localhost')
+connection = pika.BlockingConnection(connection_parameters)
+channel = connection.channel()
+channel.exchange_declare(exchange='pubsub', exchange_type=ExchangeType.fanout)
+queue = channel.queue_declare(queue='', exclusive=True)
+channel.queue_bind(exchange='pubsub', queue=queue.method.queue)
+channel.basic_consume(
+    queue=queue.method.queue, auto_ack=True, on_message_callback=on_message_received)
+
+print('Starting consuming')
+channel.start_consuming()

41746e0bd51adf21ba48672327f04e0162110a87
ervand7 <ervand7@mail.ru>
2022-03-13 12:59:49 +0300

    13.03.2022

--- Django/lessons from selfedu/coolsite/coolsite/urls.py
+++ Django/lessons from selfedu/coolsite/coolsite/urls.py
@@ -1,4 +1,4 @@
-"""MTV URL Configuration
+"""coolsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from coolsite import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('women.urls')),
 ]
+
+# handler404 = pageNotFound  #  если Debug = False
+# also there are handler403, handler400
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

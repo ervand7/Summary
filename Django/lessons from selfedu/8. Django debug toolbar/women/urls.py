8fbe61b37547ed4780077bb572d76738f210145a
ervand7 <ervand7@mail.ru>
2022-04-04 19:41:51 +0300

    04.04.2022

---
+++ Django/lessons from selfedu/8. Django debug toolbar/women/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, re_path
+
+from .views import *
+
+urlpatterns = [
+    path('', WomenHome.as_view(), name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', AddPage.as_view(), name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
+]

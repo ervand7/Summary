bc2adf73c778d088063c2046c7766761825b6b1f
ervand7 <ervand7@mail.ru>
2022-05-31 07:28:28 +0300

    31.05.2022

---
+++ Algorithms and data structures/codewars/Did you mean.py
@@ -0,0 +1,46 @@
+import re
+from itertools import chain, combinations
+from typing import Iterable
+
+
+class Dictionary:
+    def __init__(self, words) -> None:
+        self.words = words
+
+    @staticmethod
+    def find_combinations(data: list) -> list:
+        result: chain = chain.from_iterable(
+            combinations(data, i) for i in range(len(data) + 1)
+        )
+        result: list = [item for item in result if len(item) > 1]
+        return result
+
+    def find_most_similar(self, term: str) -> str:
+        changes = 100
+        closest_word = str()
+        for word in self.words:
+            if len(self.words) > len(term):
+                longer = word
+                shorter = term
+            else:
+                longer = term
+                shorter = word
+            letters_in_longer = [letter for letter in shorter if letter in longer]
+
+            letters_combinations = self.find_combinations(letters_in_longer)
+            for i in letters_combinations:  # type: tuple
+                regex = str()
+                for let in i:
+                    regex += r'[A-Za-z]*' + re.escape(let)
+                regex += r'[A-Za-z]*'
+                if all([
+                    re.search(regex, longer),
+                    (len(longer) - len(i)) < changes]
+                ):
+                    changes = len(longer) - len(i)
+                    closest_word = word
+        return closest_word
+
+
+words_list = ['cherry', 'pineapple', 'melon', 'strawberry', 'raspberry']
+print(Dictionary(words_list).find_most_similar('strawbery'))
---
+++ Algorithms and data structures/codewars/Make a spiral.py
@@ -0,0 +1,36 @@
+from itertools import cycle
+from pprint import pprint
+
+
+def spiralize(size: int):
+    directions: cycle = cycle(
+        ((1, 0), (0, 1), (-1, 0), (0, -1))
+    )
+
+    spiral = [[0] * size for _ in range(size)]
+    pprint(spiral)
+    x, y = 0, 0
+    direction_changes = 0
+
+    while direction_changes < size:
+        change_x, change_y = next(directions)
+
+        while y + change_y in range(size) and x + change_x in range(size):
+            spiral[y][x] = 1
+
+            step: bool = y + (change_y * 2) in range(size) \
+                and x + (change_x * 2) in range(size)
+            index_y: int = y + (change_y * 2)
+            index_x: int = x + (change_x * 2)
+            res = step and spiral[index_y][index_x] == 1
+            if res:
+                break
+
+            y += change_y
+            x += change_x
+        direction_changes += 1
+
+    return spiral
+
+
+pprint(spiralize(20))

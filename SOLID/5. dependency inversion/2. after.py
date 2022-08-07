08b2b812aafe46afa39d8c24bd73c1f6de4f51f0
ervand7 <ervand7@mail.ru>
2022-08-07 11:48:54 +0300

    07.08.2022

---
+++ SOLID/5. dependency inversion/2. after.py
@@ -0,0 +1,27 @@
+from abc import ABC
+
+
+class CurrencyConverter(ABC):
+    def convert(self, from_currency, to_currency, amount) -> float:
+        pass
+
+
+class FXConverter(CurrencyConverter):
+    def convert(self, from_currency, to_currency, amount) -> float:
+        print('Converting currency using FX API')
+        print(f'{amount} {from_currency} = {amount * 1.2} {to_currency}')
+        return amount * 2
+
+
+class App:
+    def __init__(self, converter: CurrencyConverter):
+        self.converter = converter
+
+    def start(self):
+        self.converter.convert('EUR', 'USD', 100)
+
+
+if __name__ == '__main__':
+    converter = FXConverter()
+    app = App(converter)
+    app.start()  # 100 EUR = 120.0 USD

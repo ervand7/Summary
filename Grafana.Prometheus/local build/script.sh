ef634ca87d49ef4d983e63f8b9be4f4cd85b58dc
ervand7 <ervand7@mail.ru>
2022-08-18 14:38:16 +0300

    18.08.2022

--- Grafana.Prometheus/local build/script.sh
+++ Grafana.Prometheus/local build/script.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-# while выполняет код пока условие верно. А until наоборот
-
 n=1
 while [ $n -lt 400 ]; do
   echo "Hello 3.14" | curl --data-binary @- http://localhost:9091/metrics/job/prometeus

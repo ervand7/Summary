ff4241d0dffd3855298d72628f2e77a99b561943
ervand7 <ervand7@mail.ru>
2022-10-23 22:23:47 +0400

    23.10.2022

--- Go/yandex.practicum/12. Multithreading/Draft.go
+++ Go/yandex.practicum/12. Multithreading/Draft.go
@@ -3,118 +3,22 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-// return channel for input numbers
-func getInputChan() <-chan int {
-	// make return channel
-	input := make(chan int, 100)
-
-	// sample numbers
-	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-	// run goroutine
-	go func() {
-		for num := range numbers {
-			input <- num
-		}
-		// close channel once all numbers are sent to channel
-		close(input)
-	}()
-
-	return input
-}
-
-// returns a channel which returns square of numbers
-func getSquareChan(input <-chan int) <-chan int {
-	// make return channel
-	output := make(chan int, 100)
-
-	// run goroutine
-	go func() {
-		// push squares until input channel closes
-		for num := range input {
-			output <- num * num
-		}
-
-		// close output channel once for loop finishes
-		close(output)
-	}()
-
-	return output
-}
-
-// returns a merged channel of `outputsChan` channels
-// this produce fan-in channel
-// this is variadic function
-func merge(outputsChan ...<-chan int) <-chan int {
-	// create a WaitGroup
-	var wg sync.WaitGroup
-
-	// make return channel
-	merged := make(chan int, 100)
-
-	// increase counter to number of channels `len(outputsChan)`
-	// as we will spawn number of goroutines equal to number of channels received to merge
-	wg.Add(len(outputsChan))
-
-	// function that accept a channel (which sends square numbers)
-	// to push numbers to merged channel
-	output := func(sc <-chan int) {
-		// run until channel (square numbers sender) closes
-		for sqr := range sc {
-			merged <- sqr
-		}
-		// once channel (square numbers sender) closes,
-		// call `Done` on `WaitGroup` to decrement counter
-		wg.Done()
-	}
-
-	// run above `output` function as groutines, `n` number of times
-	// where n is equal to number of channels received as argument the function
-	// here we are using `for range` loop on `outputsChan` hence no need to manually tell `n`
-	for _, optChan := range outputsChan {
-		go output(optChan)
-	}
-
-	// run goroutine to close merged channel once done
-	go func() {
-		// wait until WaitGroup finishes
-		wg.Wait()
-		close(merged)
-	}()
-
-	return merged
-}
-
 func main() {
-	// step 1: get input numbers channel
-	// by calling `getInputChan` function, it runs a goroutine which sends number to returned channel
-	chanInputNums := getInputChan()
-
-	// step 2: `fan-out` square operations to multiple goroutines
-	// this can be done by calling `getSquareChan` function multiple times where individual function call returns a channel which sends square of numbers provided by `chanInputNums` channel
-	// `getSquareChan` function runs goroutines internally where squaring operation is ran concurrently
-	chanOptSqr1 := getSquareChan(chanInputNums)
-	chanOptSqr2 := getSquareChan(chanInputNums)
-
-	// step 3: fan-in (combine) `chanOptSqr1` and `chanOptSqr2` output to merged channel
-	// this is achieved by calling `merge` function which takes multiple channels as arguments
-	// and using `WaitGroup` and multiple goroutines to receive square number, we can send square numbers
-	// to `merged` channel and close it
-	chanMergedSqr := merge(chanOptSqr1, chanOptSqr2)
-
-	// step 4: let's sum all the squares from 0 to 9 which should be about `285`
-	// this is done by using `for range` loop on `chanMergedSqr`
-	sqrSum := 0
-
-	// run until `chanMergedSqr` or merged channel closes
-	// that happens in `merge` function when all goroutines pushing to merged channel finishes
-	// check line no. 86 and 87
-	for num := range chanMergedSqr {
-		sqrSum += num
-	}
-
-	// step 5: print sum when above `for loop` is done executing which is after `chanMergedSqr` channel closes
-	fmt.Println("Sum of squares between 0-9 is", sqrSum)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		time.Sleep(50 * time.Millisecond)
+		fmt.Print(1)
+		wg.Done()
+	}()
+	wg.Add(-1)
+	go func() {
+		fmt.Print(2)
+		wg.Done()
+	}()
+	wg.Wait()
+	fmt.Print(3)
 }
--- Go/yandex.practicum/14. multithreading patterns/Draft.go
+++ Go/yandex.practicum/14. multithreading patterns/Draft.go
@@ -2,17 +2,27 @@ package main
 
 import "fmt"
 
-func CheckArrayElems(arr map[string]string) {
-	for key, val := range arr {
-		if len(val) == 5 {
-			arr[key] = "hello" + "/" + val
-		}
-	}
+func f(a []int) {
+	a[1] = 5
+}
+
+func f2(a []int) {
+	a = append(a, 5)
 }
 
 func main() {
-	arr := map[string]string{"qwert": "qwert", "wert1": "qwert", "wert2": "qwert", "wert3": "qwert", "wert4": "qwert"}
-	CheckArrayElems(arr)
-	fmt.Println(arr)
+	a := []int{1, 2, 3}
+	f2(a)
+	fmt.Println(a) // [1 2 3] - исходный слайс не изменился,
+	// так как append возвращает новый объект
 
+	f(a)
+	fmt.Println(a) // [1 5 3] - исходный слайс изменился,
+	// так как слайс переданный в функцию, передается как ссылка
+
+	a = []int{1, 2, 3}
+	b := a[:2:2] // сокращаем и len и cap
+	f2(b)
+	fmt.Println(b) // [1 2]
+	fmt.Println(a) // [1 2 3]
 }

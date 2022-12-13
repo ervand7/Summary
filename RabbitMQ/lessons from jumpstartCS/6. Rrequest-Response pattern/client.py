e1a6eb4c15087ac414786087450e904792f0be91
ervand7 <ervand7@mail.ru>
2022-12-14 00:12:58 +0400

    13.12.2022

--- RabbitMQ/lessons from jumpstartCS/6. Rrequest-Response pattern/client.py
+++ RabbitMQ/lessons from jumpstartCS/6. Rrequest-Response pattern/client.py
@@ -1,30 +1,33 @@
-import pika
 import uuid
 
+import pika
 
-def on_reply_message_received(ch, method, properties, body):
+
+def callback(ch, method, properties, body):
     print(f"reply received: {body}")
 
 
-connection_parameters = pika.ConnectionParameters('localhost')
-connection = pika.BlockingConnection(connection_parameters)
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 reply_queue = channel.queue_declare(queue='', exclusive=True)
-channel.basic_consume(queue=reply_queue.method.queue,
-                      auto_ack=True,
-                      on_message_callback=on_reply_message_received)
+channel.basic_consume(
+    queue=reply_queue.method.queue,
+    auto_ack=True,
+    on_message_callback=callback
+)
 
 channel.queue_declare(queue='request-queue')
-cor_id = str(uuid.uuid4())
-print(f"Sending Request: {cor_id}")
+request_id = str(uuid.uuid4())
+print(f"Sending Request: {request_id}")
 
 channel.basic_publish(
     exchange='',
     routing_key='request-queue',
     properties=pika.BasicProperties(
         reply_to=reply_queue.method.queue,
-        correlation_id=cor_id),
-    body='Can I request a reply?')
+        correlation_id=request_id),
+    body='Can I request a reply?'
+)
 
 print("Starting Client")
 channel.start_consuming()
--- RabbitMQ/lessons from jumpstartCS/6. Rrequest-Response pattern/server.py
+++ RabbitMQ/lessons from jumpstartCS/6. Rrequest-Response pattern/server.py
@@ -1,20 +1,23 @@
 import pika
 
 
-def on_request_message_received(ch, method, properties, body):
+def callback(ch, method, properties, body):
     print(f"Received Request: {properties.correlation_id} with body: {body}")
-    ch.basic_publish(exchange='',
-                     routing_key=properties.reply_to,
-                     body=f'Hey its your reply to {properties.correlation_id}')
+    ch.basic_publish(
+        exchange='',
+        routing_key=properties.reply_to,
+        body=f'Hey its your reply to {properties.correlation_id}'
+    )
 
 
-connection_parameters = pika.ConnectionParameters('localhost')
-connection = pika.BlockingConnection(connection_parameters)
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='request-queue')
-channel.basic_consume(queue='request-queue',
-                      auto_ack=True,
-                      on_message_callback=on_request_message_received)
+channel.basic_consume(
+    queue='request-queue',
+    auto_ack=True,
+    on_message_callback=callback
+)
 
 print("Starting Server")
 channel.start_consuming()

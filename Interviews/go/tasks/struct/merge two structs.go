97437660fee29991d88d5c1b9c71ffb4a6ebbcac
ervand7 <ervand7@mail.ru>
2023-01-23 20:32:49 +0400

    23.01.2023

---
+++ Interviews/go/tasks/struct/merge two structs.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+type User struct {
+	Id            int
+	Name          string
+	OfficeId      int
+	OfficeAddress string // пусто
+}
+
+type Office struct {
+	Id      int
+	Address string
+}
+
+func main() {
+	users := []User{
+		{
+			Id:       1,
+			Name:     "User1",
+			OfficeId: 1,
+		},
+		{
+			Id:       2,
+			Name:     "User2",
+			OfficeId: 2,
+		},
+	}
+
+	offices := []Office{
+		{
+			Id:      1,
+			Address: "Address1",
+		},
+		{
+			Id:      2,
+			Address: "Address2",
+		},
+	}
+
+	hashTable := make(map[int]string)
+	for _, off := range offices {
+		hashTable[off.Id] = off.Address
+	}
+
+	for idx := range users {
+		users[idx].OfficeAddress = hashTable[users[idx].OfficeId]
+	}
+
+	fmt.Printf("%+v", users)
+}

0e6987ade829e0476c8bd6b2f7996097748c5580
ervand7 <ervand7@mail.ru>
2023-02-06 17:04:13 +0400

    06.02.2023

--- OOP/magic methods/__getitem__ __setitem__ __delitem__.py
+++ OOP/magic methods/__getitem__ __setitem__ __delitem__.py
@@ -2,34 +2,42 @@ class Vector:
     def __init__(self, *args):
         self.values = list(args)
 
-    def __getitem__(self, item: int):  # item in this case means index from variable self.values
-        print('__getitem__ called')
-        if 0 <= item <= len(self.values):
+    def __getitem__(self, item: int):
+        """
+        Автоматически вызывается, когда мы обращаемся к экземпляру класса как к коллекции
+        и пытаемся получить какое-то значение по ключу
+        """
+        if 0 <= item < len(self.values):
             return self.values[item]
         else:
-            raise IndexError('Index is out of range our collection')
+            raise IndexError('Index is out of range')
 
-    def __setitem__(self, key: int, value):  # where 'key' is index, which you want to change, 'value' is a new value
-        print('__setitem__ called')
-        if 0 <= key <= len(self.values):
+    def __setitem__(self, key: int, value):
+        """
+        Автоматически вызывается, когда мы обращаемся к экземпляру класса как к коллекции
+        и пытаемся задать какое-то значение по ключу
+        """
+        if 0 <= key < len(self.values):
             self.values[key] = value
         else:
-            raise IndexError('Index is out of range our collection')
+            raise IndexError('Index is out of range')
 
     def __delitem__(self, key):
-        print('__delitem__ called')
-        if 0 <= key <= len(self.values):
+        """
+        Автоматически вызывается, когда мы обращаемся к экземпляру класса как к коллекции
+        и пытаемся удалить какое-то значение по ключу
+        """
+        if 0 <= key < len(self.values):
             del self.values[key]
         else:
-            raise IndexError('Index is out of range our collection')
+            raise IndexError('Index is out of range')
 
 
 v = Vector(1, 2, 3, 4, 5)
-print(v[3])
+print(v[3])  # 4
 
 v[3] = 155
-print(list(v))
+print(list(v))  # [1, 2, 3, 155, 5]
 
 del v[0]
-
-print(list(v))
+print(list(v))  # [2, 3, 155, 5]

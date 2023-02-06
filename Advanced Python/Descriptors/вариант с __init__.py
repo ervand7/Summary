ef34f72f2fdc5cbbc0e2635b04e4b0755fbecc4c
ervand7 <ervand7@mail.ru>
2023-02-06 15:53:39 +0400

    06.02.2023

--- Advanced Python/Descriptors/вариант с __init__.py
+++ Advanced Python/Descriptors/вариант с __init__.py
@@ -1,15 +1,23 @@
+from typing import Type
+
+
 class Descriptor:
-    """Создаем дескриптор согласно API документации."""
-    def __init__(self, name):
+    """Создаем дескриптор с __init__ согласно API документации."""
+
+    def __init__(self, name: str):
+        print('__init__ called')
         self.__name = name
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: Type['Point'], owner: 'Point'):
+        print('getter called')
         return instance.__dict__[self.__name]
 
-    def __set__(self, instance, value):
+    def __set__(self, instance: Type['Point'], value: int):
+        print('setter called')
         instance.__dict__[self.__name] = value * value
 
-    def __delete__(self, obj):
+    def __delete__(self, obj: Type['Point']):
+        print('deleter called')
         del obj.__dict__[self.__name]
 
 
@@ -19,16 +27,39 @@ class Point:
     # в классе дескриптора (Descriptor)
     coord_x = Descriptor('descriptor for coord_x')
     coord_y = Descriptor('descriptor for coord_y')
-    # вот если сейчас мы пропишем строку:
     coord_n = Descriptor('descriptor for coord_n')
-    # то мы по сути избавляемся от того, чтобы прописывать геттер, сеттер и делиттер для coord_n
+
+    # таким образом мы создали геттеры, сеттеры и делитеры для coord_x, coord_y, coord_n
 
     def __init__(self, x=0, y=0):
         self.coord_x = x
         self.coord_y = y
 
 
-a = Point(1, 2)
-b = Point(10, 20)
-print(a.coord_x, a.coord_y)
-print(b.coord_x, b.coord_y)
+p = Point(1, 2)
+# __init__ called
+# __init__ called
+# __init__ called
+
+p.coord_x = 1
+print(p.coord_x)
+# setter called
+# getter called
+# 1
+
+p.coord_y = 2
+print(p.coord_y)
+# setter called
+# getter called
+# 4
+
+p.coord_n = 3
+print(p.coord_n)
+# setter called
+# getter called
+# 9
+
+print(p.__dict__)  # {'descriptor for coord_x': 1, 'descriptor for coord_y': 4, 'descriptor for coord_n': 9}
+
+del p.coord_n
+# deleter called
--- Advanced Python/Descriptors/вариант с __set_name.py
+++ Advanced Python/Descriptors/вариант с __set_name.py
@@ -1,29 +1,66 @@
+from typing import Type
+
+
 class Descriptor:
-    """Создаем дескриптор согласно API документации."""
-    def __set_name__(self, owner, name):
-        print(name)
+    """Создаем дескриптор с __set_name__ согласно API документации."""
+
+    def __set_name__(self, owner: 'Point', name: str):
+        """
+        Какое отличие от создания дескриптора через __init__:
+        1) при присваивании нам уже ничего не нужно передавать в класс-дескриптор: param=Descriptor()
+        дескриптор автоматически определит name
+        2) он также автоматически вызывается при присваивании
+        """
+        print('__set_name__ called')
         self.__name = name
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: Type['Point'], owner: 'Point'):
+        print('getter called')
         return instance.__dict__[self.__name]
 
-    def __set__(self, instance, value):
-        instance.__dict__[self.__name] = value
+    def __set__(self, instance: Type['Point'], value: int):
+        print('setter called')
+        instance.__dict__[self.__name] = value * value
 
-    def __delete__(self, obj):
+    def __delete__(self, obj: Type['Point']):
+        print('deleter called')
         del obj.__dict__[self.__name]
 
 
 class Point:
     coord_x = Descriptor()
     coord_y = Descriptor()
+    coord_n = Descriptor()
 
     def __init__(self, x=0, y=0):
         self.coord_x = x
         self.coord_y = y
 
 
-a = Point(1, 2)
-b = Point(10, 20)
-print(a.coord_x, a.coord_y)
-print(b.coord_x, b.coord_y)
+p = Point(1, 2)
+# __set_name__ called
+# __set_name__ called
+# __set_name__ called
+
+p.coord_x = 1
+print(p.coord_x)
+# setter called
+# getter called
+# 1
+
+p.coord_y = 2
+print(p.coord_y)
+# setter called
+# getter called
+# 4
+
+p.coord_n = 3
+print(p.coord_n)
+# setter called
+# getter called
+# 9
+
+print(p.__dict__)  # {'coord_x': 1, 'coord_y': 4, 'coord_n': 9}
+
+del p.coord_n
+# deleter called

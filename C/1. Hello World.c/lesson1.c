0e2d4e8ede8505bd7127338303ab3afe10783f69
ervand7 <ervand7@mail.ru>
2023-03-26 11:45:22 +0300

    26.03.2023

--- C/1. Hello World.c/lesson1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("Hello, world\n");
-    return 0;
-}
-
-// /usr/bin/gcc -g "/Users/ervand_agadzhanyan/Desktop/Summary/C/1. Hello World.c/lesson1.c" -o "/Users/ervand_agadzhanyan/Desktop/Summary/C/1. Hello World.c/lesson1"

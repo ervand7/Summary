4f4cdfb33e32f46d0080e70b2568c2f1b23649e0
ervand7 <ervand7@mail.ru>
2023-04-26 17:55:40 +0300

    26.04.2023

--- _ Interviews/go/Draft.go
+++ _ Interviews/go/Draft.go
@@ -2,13 +2,11 @@ package main
 
 import "fmt"
 
+func unintuitive() (value string) {
+	defer func() { value = "На самом деле" }() // круглые скобки в конце означают, что функция вызывается
+	return "Казалось бы"
+}
+
 func main() {
-	type A struct {
-		IntField int
-	}
-
-	p := &A{}
-	(*p).IntField = 42 // вместо (*p).IntField = 42
-
-	fmt.Printf("%#v", p)
+	fmt.Println(unintuitive())
 }

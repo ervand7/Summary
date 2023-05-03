4128f7b8f49a98a534955d4186854742c3d5d4ac
ervand7 <ervand7@mail.ru>
2023-05-03 19:46:57 +0300

    03.05.2023

--- _ Interviews/go/Draft.go
+++ _ Interviews/go/Draft.go
@@ -2,12 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
-	var ch chan int
+/*
+Это отличный пример, чтобы посмотреть в дебаггере, как работает буффер
+(если канал сделать буфферизированным) и как работает программа с
+небуфферихзированным каналом
+*/
 
-	go func() {
-		ch <- 7
-	}()
-	v := <-ch
-	fmt.Println(v)
+func fibonacci(n int, ch chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		fmt.Println("=============1  ", x)
+		ch <- x
+		x, y = y, x+y
+	}
+
+	close(ch)
+}
+
+func main() {
+	ch := make(chan int, 105)
+	go fibonacci(15, ch)
+
+	for i := range ch {
+		fmt.Printf("%d ", i)
+		fmt.Println("=============2")
+	}
 }

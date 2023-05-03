fa22f8cd2fc77621964a29bdd73e95a306ca3f2c
ervand7 <ervand7@mail.ru>
2023-05-03 23:01:26 +0300

    03.05.2023

---
+++ _ Interviews/go/theory/chan/4. select case (read two times).go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+/*
+Вывод будет 135790. Потому что при `for _ = range ch` вычитывается значение
+из канала, а также при `case i := <-ch:`. Последнее значение (0) будет
+уже считано из закрытого канала.
+*/
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		for i := 0; i < 11; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+	for _ = range ch {
+		select {
+		case i := <-ch:
+			fmt.Print(i)
+		}
+	}
+}

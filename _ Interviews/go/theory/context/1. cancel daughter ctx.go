9c0cab591c6f6787f08586418ca0e2ec8863e38a
ervand7 <ervand7@mail.ru>
2023-05-04 14:05:47 +0300

    04.05.2023

--- _ Interviews/go/theory/context/1. cancel daughter ctx.go
+++ _ Interviews/go/theory/context/1. cancel daughter ctx.go
@@ -3,15 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
+	wg := &sync.WaitGroup{}
 	ctx, cancel := context.WithCancel(context.Background())
-	time.AfterFunc(time.Millisecond, cancel)
 
-	func(ctx context.Context) {
-		for i := 0; i < 1000000; i++ {
+	wg.Add(1)
+	go func(ctx context.Context, wg *sync.WaitGroup) {
+		defer wg.Done()
+		i := 0
+		for {
 			select {
 			case <-ctx.Done():
 				fmt.Println("Context was canceled")
@@ -19,6 +23,10 @@ func main() {
 			default:
 				fmt.Println(i)
 			}
+			i++
 		}
-	}(ctx)
+	}(ctx, wg)
+
+	time.AfterFunc(20*time.Millisecond, cancel)
+	wg.Wait()
 }

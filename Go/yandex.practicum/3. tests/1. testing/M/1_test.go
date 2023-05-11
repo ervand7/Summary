a9a077dc5988fdfab4025ad76f07ac9467ea61b5
ervand7 <ervand7@mail.ru>
2023-05-11 21:49:34 +0300

    11.05.2023

---
+++ Go/yandex.practicum/3. tests/1. testing/M/1_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	log.Println("Do stuff BEFORE the tests!")
+	exitVal := m.Run()
+	log.Println("Do stuff AFTER the tests!")
+
+	os.Exit(exitVal)
+}
+
+func TestA(t *testing.T) {
+	log.Println("TestA running")
+}
+
+func TestB(t *testing.T) {
+	log.Println("TestB running")
+}

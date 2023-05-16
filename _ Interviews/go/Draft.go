c818d06678c4ce9b5d2cb1b6e0788cf3c2d13b1e
ervand7 <ervand7@mail.ru>
2023-05-16 11:27:29 +0300

    16.05.2023

--- _ Interviews/go/Draft.go
+++ _ Interviews/go/Draft.go
@@ -2,24 +2,8 @@ package main
 
 import "fmt"
 
-// fib returns a channel which transports fibonacci numbers
-func fib(length int) <-chan int {
-	// make buffered channel
-	c := make(chan int, length)
-
-	// run generation concurrently
-	for i, j := 0, 1; i < length; i, j = i+j, i {
-		c <- i
-	}
-	close(c)
-
-	// return channel
-	return c
-}
-
 func main() {
-	// read 10 fibonacci numbers from channel returned by `fib` function
-	for fn := range fib(10) {
-		fmt.Println("Current fibonacci number is", fn)
+	for i := 0; i < 100; i++ {
+		fmt.Println(i % 2)
 	}
 }

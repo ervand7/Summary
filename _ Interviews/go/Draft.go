2835f4b81b97ef3f7831cd08ae728dcceada5fdd
ervand7 <ervand7@mail.ru>
2023-05-21 10:44:31 +0300

    21.05.2023

--- _ Interviews/go/Draft.go
+++ _ Interviews/go/Draft.go
@@ -1,24 +1,2 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-)
-
-func main() {
-	ch := make(chan []byte)
-	go func(recv chan []byte) {
-		resp, _ := http.Get("https://example.com")
-		defer resp.Body.Close()
-		jsonBytes, _ := ioutil.ReadAll(resp.Body)
-		recv <- jsonBytes
-	}(ch)
-
-	fmt.Println("Program continues...")
-	// Do other things
-
-	// Then wait for HTTP response to come back
-	result := <-ch
-	fmt.Println("Done, result is", string(result))
-}

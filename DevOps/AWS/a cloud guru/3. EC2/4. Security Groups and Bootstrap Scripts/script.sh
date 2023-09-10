9edac3bbeb0088a37fbb016082c377f72769764a
ervand7 <ervand7@mail.ru>
2023-09-10 12:24:32 +0200

    10.09.2023

--- DevOps/AWS/a cloud guru/3. EC2/4. Security Groups and Bootstrap Scripts/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-cd /var/www/html
-echo "<html><body><h1>Hello Cloud Gurus</h1></body></html>" > index.html
--- DevOps/AWS/a cloud guru/3. EC2/5. Metadata/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-cd /var/www/html
-echo "<html><body><h1>My IP is" > index.html
-curl http://169.254.169.254/latest/meta-data/public-ipv4 >> index.html
-echo "</h1></body></html>" >> index.html

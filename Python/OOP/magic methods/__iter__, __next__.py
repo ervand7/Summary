dfa11cc39ef4d77ff999d1e66cdcc74bd373d897
ervand7 <ervand7@mail.ru>
2023-09-28 21:59:00 +0200

    28.09.2023

--- Python/OOP/magic methods/__iter__, __next__.py
+++ Python/OOP/magic methods/__iter__, __next__.py
@@ -1,4 +1,5 @@
-# реализуем арифметическую последовательность вещественных чисел
+# for iter() and next()
+
 class FRange:
     def __init__(self, start=0.0, stop=0.0, step=1.0):
         self.start = start
@@ -6,6 +7,7 @@ class FRange:
         self.step = step
 
     def __next__(self):
+        print("__next__ called", end=" ")
         if (self.start + self.step) <= self.stop:
             current_value = self.start
             self.start += self.step
@@ -13,16 +15,25 @@ class FRange:
         raise StopIteration
 
     def __iter__(self):
+        """
+        Вызовется при iter(instance).
+        Без этого метода нельзя использовать instance при обходе через цикл.
+        Однако можно обойтись без него при использовании next(instance), но лучше все таки
+        всегда его определять вместе с __next__.
+        """
         return self
 
 
 fr = FRange(0, 2, 0.5)
 print(type(fr))  # <class '__main__.FRange'>
 
-print(next(fr))  # 0.0
-print(next(fr))  # 0.5
-print(next(fr))  # 1.0
-print(next(fr))  # 1.5
+print(fr)  # <__main__.FRange object at 0x7f92f8175fd0>
+print(iter(fr))  # <__main__.FRange object at 0x7f87b8175fd0>
+
+print(next(fr))  # __next__ called 0.0
+print(next(fr))  # __next__ called 0.5
+print(next(fr))  # __next__ called 1.0
+print(next(fr))  # __next__ called 1.5
 
 # ничего не выведется, так как мы уже обходили итератор fr до этого 1 раз
 for x in fr:

15e9149273b7fb23e6e3b60d3e139b2e47660f5f
ervand7 <ervand7@mail.ru>
2023-10-01 18:52:14 +0200

    01.10.2023

--- Python/Python versions/что нового в 3.10/1. pattern matching/5. use in classes.py
+++ Python/Python versions/что нового в 3.10/1. pattern matching/5. use in classes.py
@@ -10,15 +10,15 @@ class UserInput:
         self.value = value
 
 
-def run_horizontally(user_input: UserInput | dict):
+def run(user_input: UserInput | dict):
     match user_input:
         # фишка pattern matching в данном примере в том, что для проверки
         # этого условия не будет создаваться отдельный экземпляр класса UserInput
         # а будет проверяться только паттерн. Это мы можем вычислить благодаря print('__init__ called').
-        # Также, что тут идет не вызов конструктора, мы можем понять, если передадим в case
-        # не все требуемые обязательные атрументы класса, например:
+        # Также, то, что тут идет не вызов конструктора, мы можем понять, если передадим
+        # в case не все требуемые обязательные аргументы класса, например:
         # case UserInput(action='left' | 'right'):
-        # И никакой ошибки не будет6 паттерн отработает.
+        # И никакой ошибки не будет, паттерн отработает.
         case UserInput(action='left' | 'right', value=value):
             print(f'Moving horizontally on {value} px')
         case {'action': 'left' | 'right', 'value': value}:
@@ -30,12 +30,7 @@ def run_horizontally(user_input: UserInput | dict):
             print('Error')
 
 
-input1 = UserInput('left', 150)
-input2 = {'action': 'right', 'value': 300}
-input3 = UserInput('top', 20)
-input4 = UserInput('вправо', 151)
-
-run_horizontally(input1)  # Moving horizontally on 150 px
-run_horizontally(input2)  # Moving horizontally on 300 px
-run_horizontally(input3)  # Error
-run_horizontally(input4)  # Перемещение по горизонтали на 151 px
+run(UserInput('left', 150))  # Moving horizontally on 150 px
+run({'action': 'right', 'value': 300})  # Moving horizontally on 300 px
+run(UserInput('top', 20))  # Error
+run(UserInput('вправо', 151))  # Перемещение по горизонтали на 151 px

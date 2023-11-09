175fb7803d4b775dd77cc1ad4d235e6a71de7646
ervand7 <ervand7@mail.ru>
2023-11-09 22:34:26 +0100

    09.11.2023

--- Algorithms and data structures/Data structures/3. Queue/7. heap-based priority queue.py
+++ Algorithms and data structures/Data structures/3. Queue/7. heap-based priority queue.py
@@ -22,10 +22,26 @@ class PriorityQueue:
 if __name__ == "__main__":
     priority_queue = PriorityQueue()
 
-    priority_queue.push("Task 1", 5)
-    priority_queue.push("Task 2", 3)
+    priority_queue.push("Task 1", 9)
+    priority_queue.push("Task 2", 8)
     priority_queue.push("Task 3", 7)
+    priority_queue.push("Task 4", 6)
+    priority_queue.push("Task 5", 5)
+    priority_queue.push("Task 6", 4)
+    priority_queue.push("Task 7", 3)
+    priority_queue.push("Task 8", 2)
+    priority_queue.push("Task 9", 1)
 
     while not priority_queue.is_empty():
         task = priority_queue.pop()
         print(f"Processed: {task}")
+
+# Processed: Task 9
+# Processed: Task 8
+# Processed: Task 7
+# Processed: Task 6
+# Processed: Task 5
+# Processed: Task 4
+# Processed: Task 3
+# Processed: Task 2
+# Processed: Task 1

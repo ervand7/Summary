761e3cb9cd6fe5a5d203991b14e924cdb7534a7e
ervand7 <ervand7@mail.ru>
2023-12-03 21:40:44 +0100

    3.12.2023

--- Algorithms and data structures/Algorithms/Tasks/Leetcode/10. Search Insert Position.py
+++ Algorithms and data structures/Algorithms/Tasks/Leetcode/10. Search Insert Position.py
@@ -1,7 +1,38 @@
 from typing import List
 
 
+# Given a sorted array of distinct integers and a target value, return
+# the index if the target is found. If not, return the index where it would be
+# if it were inserted in order.
+#
+# You must write an algorithm with O(log n) runtime complexity.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+# Example 2:
+#
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+# Example 3:
+#
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# nums contains distinct values sorted in ascending order.
+# -104 <= target <= 104
+
+
 # my solution
+# O(n)
 def search_insert_idx(nums: List[int], target: int) -> int:
     if nums[-1] < target:
         return len(nums)
@@ -10,4 +41,23 @@ def search_insert_idx(nums: List[int], target: int) -> int:
             return i
 
 
-print(search_insert_idx(nums=[1, 3, 5, 6], target=7))
+print(search_insert_idx(nums=[1, 3, 5, 6], target=5))  # 2
+
+
+# ChatGPT solution
+# O(log n)
+def search_insert_idx(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left
+
+
+print(search_insert_idx(nums=[1, 3, 5, 6], target=5))  # 2

b1ad73ebe4422e2cb4d7bfcfd4aec988b6a1bb4f
ervand7 <ervand7@mail.ru>
2023-12-05 21:27:55 +0100

    5.12.2023

--- Algorithms and data structures/Algorithms/Tasks/Leetcode/12. Length of Last Word.py
+++ Algorithms and data structures/Algorithms/Tasks/Leetcode/12. Length of Last Word.py
@@ -1,2 +1,70 @@
+# Given a string s consisting of words and spaces, return the length of the last
+# word in the string.
+#
+# A word is a maximal substring consisting of non-space characters only.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+# Example 2:
+#
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
+# Example 3:
+#
+# Input: s = "luffy is still joyboy"
+# Output: 6
+# Explanation: The last word is "joyboy" with length 6.
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 104
+# s consists of only English letters and spaces ' '.
+# There will be at least one word in s.
+
+# solution using build-in func
 def length_of_last_word(s: str) -> int:
     return len(s.rsplit()[-1])
+
+
+print(length_of_last_word("Hello world"))
+
+
+# my first solution
+def length_of_last_word(s: str) -> int:
+    result = 0
+    pointer = 0
+    for i in s:
+        if i != " ":
+            pointer += 1
+            result = pointer
+        elif i == " " and pointer != 0:
+            result = pointer
+            pointer = 0
+
+    return result
+
+
+print(length_of_last_word("luffy is still joyboy"))
+
+
+# my first solution: search from the end
+def length_of_last_word(s: str) -> int:
+    flag = False
+    pointer = 0
+    for i in range(len(s) - 1, -1, -1):
+        if s[i] != " ":
+            pointer += 1
+            flag = True
+        if flag is True and s[i] == " ":
+            return pointer
+    return pointer
+
+
+print(length_of_last_word("luffy is still joyboy"))
--- Algorithms and data structures/Algorithms/Tasks/Leetcode/14. Add Binary.py
+++ Algorithms and data structures/Algorithms/Tasks/Leetcode/14. Add Binary.py
@@ -1,5 +1,51 @@
+# Given two binary strings a and b, return their sum as a binary string.
+#
+#
+#
+# Example 1:
+#
+# Input: a = "11", b = "1"
+# Output: "100"
+# Example 2:
+#
+# Input: a = "1010",
+#        b = "1011"
+# Output: "10101"
+#
+#
+# Constraints:
+#
+# 1 <= a.length, b.length <= 104
+# a and b consist only of '0' or '1' characters.
+# Each string does not contain leading zeros except for the zero itself.
+
 def add_binary(a: str, b: str) -> str:
     return bin(int(a, 2) + int(b, 2))[2:]
 
 
-print(add_binary(a="11", b="1"))
+print(add_binary(a="10001010101", b="1111"))
+
+
+def add_binary(a: str, b: str) -> str:
+    result = []
+    carry = 0
+
+    i, j = len(a) - 1, len(b) - 1
+    while i >= 0 or j >= 0 or carry:
+        digit_a = int(a[i]) if i >= 0 else 0
+        digit_b = int(b[j]) if j >= 0 else 0
+
+        current_sum = digit_a + digit_b + carry
+        carry = current_sum // 2
+        current_sum %= 2
+
+        result.insert(0, str(current_sum))
+
+        i -= 1
+        j -= 1
+
+    return ''.join(result)
+
+
+print(add_binary(a="10001010101", b="1111"))
+

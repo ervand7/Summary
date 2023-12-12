e4a83d95184658acd6d8c1933664057314469491
ervand7 <ervand7@mail.ru>
2023-12-12 21:05:49 +0100

    12.12.2023

--- Algorithms and data structures/Algorithms/Tasks/Leetcode/20. Same Tree.py
+++ Algorithms and data structures/Algorithms/Tasks/Leetcode/20. Same Tree.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -8,7 +9,7 @@ class TreeNode:
         self.right = right
 
     def __str__(self):
-        return f'|{self.val}|'
+        return str(self.val)
 
     def insert(self, val):
         if self.val:
@@ -27,29 +28,24 @@ class TreeNode:
             self.val = val
 
 
-# first decision
-def isSameTree1(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    if p and q:
-        return p.val == q.val \
-               and isSameTree1(p.left, q.left) \
-               and isSameTree1(p.right, q.right)
-    return p == q
+class Solution:
+    def is_same_tree(self, p: Optional[TreeNode], q: Optional[TreeNode]):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.is_same_tree(p.left, q.left) and self.is_same_tree(p.right, q.right)
 
 
-root = TreeNode(27)
-[root.insert(i) for i in (14, 35, 10, 19, 31, 42)]
+a = TreeNode()
+b = TreeNode()
+for i in range(10):
+    a.insert(i)
+    b.insert(i)
 
-root2 = TreeNode(27)
-[root2.insert(i) for i in (14, 35, 11, 19, 31, 42)]
-print(isSameTree1(root, root2))
+print(Solution().is_same_tree(a, b))  # True
 
-
-# second decision
-def isSameTree2(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    def t(n):
-        return n and (n.val, t(n.left), t(n.right))
-
-    return t(p) == t(q)
-
-
-print(isSameTree2(root, root2))
+a.insert(55)
+print(Solution().is_same_tree(a, b))  # False

2a97986cba7dd35e0b2b27ede79b87c218ba127b
ervand7 <ervand7@mail.ru>
2023-12-20 21:08:47 +0100

    20.12.2023

--- Algorithms and data structures/Algorithms/Tasks/Leetcode/35. Majority Element.py
+++ Algorithms and data structures/Algorithms/Tasks/Leetcode/35. Majority Element.py
@@ -1,9 +1,11 @@
-def majorityElement(nums: List[int]) -> int:
-    dic = {}
-    for i in nums:
-        dic[i] = dic.get(i, 0) + 1
+from typing import List
 
-    max_value = max(dic.values())
-    for key, value in dic.items():
-        if value == max_value:
-            return key
+
+# my solution
+def majority_element(nums: List[int]) -> int:
+    threshold = len(nums) // 2
+    hash_table = {}
+    for i in nums:
+        hash_table[i] = hash_table.get(i, 0) + 1
+        if hash_table[i] > threshold:
+            return i

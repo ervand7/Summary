d2a79978ef94c3ec2745bbe0de6f7345cc361070
ervand7 <ervand7@mail.ru>
2024-01-17 18:54:38 +0100

    17.01.2024

---
+++ Algorithms and data structures/Algorithms/Tasks/Leetcode/0. Int to bin.py
@@ -0,0 +1,11 @@
+def int_to_bin(num):
+    if num == 0:
+        return "0"
+    binary = []
+    while num > 0:
+        # Append the remainder (0 or 1) to the binary list
+        binary.append(str(num % 2))
+        # Divide the number by 2
+        num = num // 2
+    # Reverse the list and join to get the binary representation
+    return ''.join(reversed(binary))

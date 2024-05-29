5860a805f087d8ea0dee5c0e36476bb9669bf73d
ervand7 <ervand7@mail.ru>
2024-05-29 19:34:16 +0200

    29.05.2024

--- Algorithms and data structures/Algorithms/Tasks/Leetcode/Easy/168. Projection Area of 3D Shapes.py
+++ Algorithms and data structures/Algorithms/Tasks/Leetcode/Easy/168. Projection Area of 3D Shapes.py
@@ -1,11 +1,10 @@
-def projectionArea(grid):
+from typing import List
+
+
+# у задачи абсолютно ужасное описания. Я так и не понял условия
+def projection_area(grid: List[List[int]]) -> int:
     xy_projection = sum(val > 0 for row in grid for val in row)
     yz_projection = sum(max(col) for col in zip(*grid))
     zx_projection = sum(max(row) for row in grid)
 
     return xy_projection + yz_projection + zx_projection
-
-
-# Test cases
-print(projectionArea([[2]]))  # Output: 5
-print(projectionArea([[1, 0], [0, 2]]))  # Output: 8

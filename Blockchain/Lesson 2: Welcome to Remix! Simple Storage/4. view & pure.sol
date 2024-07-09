139aad2c5ade5bf86cda1fbd725ba0bf5948a626
ervand7 <ervand7@mail.ru>
2024-07-09 21:00:45 +0200

    09.07.2024

---
+++ Blockchain/Lesson 2: Welcome to Remix! Simple Storage/4. view & pure.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+// function visibility specifier:
+// https://docs.soliditylang.org/en/latest/contracts.html#visibility-and-getters
+
+pragma solidity 0.8.8;
+
+contract  SimpleStorage {
+    uint public favoriteNumber;
+
+    function store(uint256 num) public {
+        favoriteNumber = num;
+    }
+
+    // view:
+    // Функции могут читать данные из блокчейна, но не могут изменять их.
+    // Например, они могут читать значения переменных состояния контракта.
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+
+    // pure:
+    // Функции не могут ни читать, ни изменять данные блокчейна.
+    // Они могут работать только с входными аргументами и локальными переменными.
+    function add() public pure returns (uint256){
+        return (1 + 1);
+    }
+}
+

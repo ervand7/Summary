8dffd4993bd837203e68414d2698ca20c459365e
ervand7 <ervand7@mail.ru>
2024-07-17 22:30:35 +0200

    17.07.2024

---
+++ Blockchain/Lesson 4: Remix Fund Me/5. Library/FundMe.sol
@@ -0,0 +1,23 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+//  SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUSD = 1 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable{
+        require(msg.value.getConversionRate() >= minimumUSD, "Didn't send enougth"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+}

c9c2c747c8a31f877657a8a9ba92c9bcd4079e76
ervand7 <ervand7@mail.ru>
2024-07-26 10:38:31 +0200

    26.07.2024

---
+++ Blockchain/Lesson 5: Ethers.js Simple  /5. Additional encryption/hh-fcc/ethers-simple-storage-fcc/deploy.js
@@ -0,0 +1,44 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  var wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  wallet = await wallet.connect(provider);
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  await contract.deployTransaction.wait(1); // wait one block
+
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
+
+  // good practice in js - to pass num as string
+  const transactionResponse = await contract.store("7");
+  const transactionReceipt = await transactionResponse.wait(1);
+  const updatedFavoriteNumber = await contract.retrieve();
+  console.log(
+    `Updated Favorite Number is: ${updatedFavoriteNumber.toString()}`
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

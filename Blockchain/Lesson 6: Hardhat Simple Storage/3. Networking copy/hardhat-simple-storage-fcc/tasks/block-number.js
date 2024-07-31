b2d680e647dfc600eefb38fefeb0cdc23aa2ca47
ervand7 <ervand7@mail.ru>
2024-07-31 19:05:01 +0200

    31.07.2024

--- Blockchain/Lesson 6: Hardhat Simple Storage/3. Networking copy/hardhat-simple-storage-fcc/tasks/block-number.js
+++
@@ -1,8 +0,0 @@
-const { task } = require("hardhat/config");
-
-task("block-number", "Prints the current block number").setAction(
-  async (taskArgs, hre) => {
-    const blockNumber = await hre.ethers.provider.getBlockNumber();
-    console.log(`Current block number: ${blockNumber}`)
-  }
-);

034cb9ffd47aba590bad78ce24aaae3a0fbee239
ervand7 <ervand7@mail.ru>
2024-08-05 17:02:22 +0200

    05.08.2024

--- Blockchain/Lesson 7: Hardhat Fund Me/hardhat-fund-me-fcc/contracts/FundMe.sol
+++ Blockchain/Lesson 7: Hardhat Fund Me/hardhat-fund-me-fcc/contracts/FundMe.sol
@@ -7,50 +7,37 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe__NotOwner();
 
+/**
+ * @title A contract for crowd funding
+ * @author Ivan Petrov
+ * @notice This contract is to de,o a sample funding contract
+ * @dev This implements price feeds for our library
+ */
 contract FundMe {
     using PriceConverter for uint256;
+
+    // good convention to start the name with i_ in immutable case
+    address public immutable i_owner;
 
     // using constant we can save gas
     uint256 public constant MINIMUM_USD = 0.1 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
-
     AggregatorV3Interface public priceFeed;
 
-    // good convention to start the name with i_ in immutable case
-    address public immutable i_owner;
-    constructor(address priceFeedAddress){
-        i_owner = msg.sender;
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
-    }
-
-    function fund() public payable{
-        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "Didn't send enougth"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
-    }
-
-    function withdraw() public onlyOwner {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
-
-        // reset the array
-        funders = new address[](0);
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
-    }
-
-    modifier onlyOwner {
-        // require(msg.sender == i_owner, "Sender is not owner!");
+    modifier onlyOwner() {
         if (msg.sender != i_owner) {
-            revert NotOwner();
+            revert FundMe__NotOwner();
         }
         _;
+    }
+
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     receive() external payable {
@@ -59,5 +46,35 @@ contract FundMe {
 
     fallback() external payable {
         fund();
+    }
+
+    /**
+     * @notice this function funds this contract
+     */
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
+            "Didn't send enougth"
+        ); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        // reset the array
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
     }
 }

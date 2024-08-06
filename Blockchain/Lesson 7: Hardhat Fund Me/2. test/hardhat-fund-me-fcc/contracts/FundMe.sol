04fd0df893d5071850efb11ba15e5c7ba2f948bc
ervand7 <ervand7@mail.ru>
2024-08-06 19:07:50 +0200

    06.08.2024

--- Blockchain/Lesson 7: Hardhat Fund Me/2. test/hardhat-fund-me-fcc/contracts/FundMe.sol
+++ Blockchain/Lesson 7: Hardhat Fund Me/2. test/hardhat-fund-me-fcc/contracts/FundMe.sol
@@ -18,7 +18,7 @@ contract FundMe {
     using PriceConverter for uint256;
 
     // State variables
-    uint256 public constant MINIMUM_USD = 50 * 10**18;
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
     address private immutable i_owner;
     address[] private s_funders;
     mapping(address => uint256) private s_addressToAmountFunded;
@@ -96,11 +96,9 @@ contract FundMe {
      *  @param fundingAddress the address of the funder
      *  @return the amount funded
      */
-    function getAddressToAmountFunded(address fundingAddress)
-        public
-        view
-        returns (uint256)
-    {
+    function getAddressToAmountFunded(
+        address fundingAddress
+    ) public view returns (uint256) {
         return s_addressToAmountFunded[fundingAddress];
     }
 
--- Blockchain/Lesson 7: Hardhat Fund Me/2. test/hardhat-fund-me-fcc/scripts/withdraw.js
+++ Blockchain/Lesson 7: Hardhat Fund Me/2. test/hardhat-fund-me-fcc/scripts/withdraw.js
@@ -1,18 +1,18 @@
-const { ethers, getNamedAccounts } = require("hardhat")
+const { ethers, getNamedAccounts } = require("hardhat");
 
 async function main() {
-  const { deployer } = await getNamedAccounts()
-  const fundMe = await ethers.getContract("FundMe", deployer)
-  console.log(`Got contract FundMe at ${fundMe.address}`)
-  console.log("Withdrawing from contract...")
-  const transactionResponse = await fundMe.withdraw()
-  await transactionResponse.wait()
-  console.log("Got it back!")
+  const { deployer } = await getNamedAccounts();
+  const fundMe = await ethers.getContract("FundMe", deployer);
+  console.log(`Got contract FundMe at ${fundMe.address}`);
+  console.log("Withdrawing from contract...");
+  const transactionResponse = await fundMe.withdraw();
+  await transactionResponse.wait(1);
+  console.log("Got it back!");
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    console.error(error);
+    process.exit(1);
+  });

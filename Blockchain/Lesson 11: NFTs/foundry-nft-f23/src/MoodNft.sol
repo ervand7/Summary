c09e3b6b32e72b36a505c174905b62a285d5d929
ervand7 <ervand7@mail.ru>
2024-10-20 12:10:33 +0200

    20.10.2024

--- Blockchain/Lesson 11: NFTs/foundry-nft-f23/src/MoodNft.sol
+++ Blockchain/Lesson 11: NFTs/foundry-nft-f23/src/MoodNft.sol
@@ -1,24 +1,3 @@
-// Layout of Contract:
-// version
-// imports
-// errors
-// interfaces, libraries, contracts
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
-
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// view & pure functions
-
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 

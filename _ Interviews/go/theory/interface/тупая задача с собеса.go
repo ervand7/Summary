a0e0568062d9d2071ad829725879ca6115563af8
ervand7 <ervand7@mail.ru>
2025-12-03 18:35:36 +0100

    03.12.2025

--- _ Interviews/go/theory/interface/тупая задача с собеса.go
+++ _ Interviews/go/theory/interface/тупая задача с собеса.go
@@ -2,13 +2,16 @@ package main
 
 import "fmt"
 
+// Because after assigning `data` to the interface, the interface is no longer nil
+// since it now contains a type (`*byte`) even though the stored value is nil.
+
 func main() {
 	var data *byte
 	var in interface{}
 
-	fmt.Println(data, data == nil) // <nil> true
-	fmt.Println(in, in == nil)     // <nil> true
+	fmt.Println(data, data == nil)
+	fmt.Println(in, in == nil)
 
 	in = data
-	fmt.Println(in, in == nil) // <nil> false
+	fmt.Println(in, in == nil)
 }

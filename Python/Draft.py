679d1eb21b43aae4a31ea8d7fa4449c13a72d738
ervand7 <ervand7@mail.ru>
2025-12-06 16:14:58 +0100

    06.12.2025

---
+++ Python/Draft.py
@@ -0,0 +1,20 @@
+import asyncio
+
+
+async def task1():
+    print("Task 1 starting")
+    await asyncio.sleep(1)
+    print("Task 1 resuming")
+
+
+async def task2():
+    print("Task 2 starting")
+    await asyncio.sleep(1)
+    print("Task 2 resuming")
+
+
+async def main():
+    await asyncio.gather(task1(), task2())
+
+
+asyncio.run(main())

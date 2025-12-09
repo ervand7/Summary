992e5589896c7759c995767d16cd197c13ba96a2
ervand7 <ervand7@mail.ru>
2025-12-09 20:43:57 +0100

    09.12.2025

--- _ Interviews/go/theory/slice/copy/1. create.go
+++ _ Interviews/go/theory/slice/copy/1. create.go
@@ -2,31 +2,18 @@ package main
 
 import "fmt"
 
-// Скопируется столько элементов, сколько может вместить dst
-// После copy адрес не меняется
-
 func main() {
 	a := []int{1, 2, 3, 4, 5}
 	b := make([]int, 5)
 	var c []int
 	d := make([]int, 10)
 
-	fmt.Printf("%p\n", &a) // 0x140000b8000
-	fmt.Printf("%p\n", &b) // 0x140000b8018
-	fmt.Printf("%p\n", &c) // 0x140000b8030
-	fmt.Printf("%p\n", &d) // 0x140000b8048
-
 	copy(b, a)
 	copy(c, a)
 	copy(d, a)
 
-	fmt.Println(a) // [1 2 3 4 5]
-	fmt.Println(b) // [1 2 3 4 5]
-	fmt.Println(c) // []
-	fmt.Println(d) // [1 2 3 4 5 0 0 0 0 0]
-
-	fmt.Printf("%p\n", &a) // 0x140000b8000
-	fmt.Printf("%p\n", &b) // 0x140000b8018
-	fmt.Printf("%p\n", &c) // 0x140000b8030
-	fmt.Printf("%p\n", &d) // 0x140000b8048
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 }

9e472ccef05a8cfdd587f097a7a598394ee0f231
ervand7 <ervand7@mail.ru>
2026-01-25 18:33:56 +0100

    25.01.2026

--- _ Interviews/go/theory/patterns/3. fan-in fan-out.go
+++ _ Interviews/go/theory/patterns/3. fan-in fan-out.go
@@ -6,44 +6,43 @@ import (
 	"time"
 )
 
-func fanOut(id int, wg *sync.WaitGroup, in <-chan int, out chan<- int) {
+func funOut(wg *sync.WaitGroup, in <-chan int, out chan<- int, workerID int) {
 	defer wg.Done()
 	for job := range in {
-		fmt.Printf("worker %d processes value %d\n", id, job)
-		time.Sleep(time.Second)
+		fmt.Printf("worker %d processes job %d\n", workerID, job)
+		time.Sleep(time.Second) //  simulate hard work
 		out <- job * job
 	}
 }
 
 func main() {
-
 	var (
 		wg           sync.WaitGroup
 		in           = make(chan int)
 		out          = make(chan int)
-		workersCount = 3
-		jobsCount    = 10
+		workersCount = 10
+		jobsCount    = 100
 	)
 
 	for i := 0; i < workersCount; i++ {
 		wg.Add(1)
-		go fanOut(i, &wg, in, out)
+		go funOut(&wg, in, out, i)
 	}
+
+	// fanIn
+	go func() {
+		for i := 0; i < jobsCount; i++ {
+			in <- i
+		}
+		close(in)
+	}()
 
 	go func() {
 		wg.Wait()
 		close(out)
 	}()
 
-	// fanIn
-	go func() {
-		for j := 0; j < jobsCount; j++ {
-			in <- j
-		}
-		close(in)
-	}()
-
-	for i := range out {
-		fmt.Printf("result %d\n", i)
+	for processedItem := range out {
+		fmt.Printf("got %d\n", processedItem)
 	}
 }

f9a002ed46186eee5e1d1fd1f24b8db4123225f4
ervand7 <ervand7@mail.ru>
2026-03-07 07:46:45 +0100

    07.03.2026

--- _ Interviews/SOLID/5. Dependency Inversion.py
+++ _ Interviews/SOLID/5. Dependency Inversion.py
@@ -4,9 +4,6 @@ from abc import ABC, abstractmethod
 High-level modules should not depend on low-level modules.
 Both should depend on abstractions.
 
-Abstractions should not depend on details.
-Details should depend on abstractions.
-
 Computer is a high-level class.
 SaveToFile / SaveToDB are low-level details.
 Computer depends only on Saver (abstraction), not on concrete classes.
